a9ed4b22df45083eba544905ed7020f3d0e840f4
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-05-20 15:56:51 +0300

    refactor in read_maze

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from maze_generator.read_maze import ReadMaze
 from solvers.BFS_Algorithm import BFS
 from maze_generator.maze import MazeNode
 from visualizations.Graph_Visualization import GraphVisualization
@@ -6,7 +7,21 @@ import matplotlib.pyplot as plt
 
 def main():
   # Representation of a graph as a dictionary
-  graph = {12: [13, 23], 13: [14, 12], 14: [15, 13, 25], 15: ['T', 14], 'T': [15], 18: [29], 20: [31], 23: [34, 12], 25: [36, 14], 29: [40, 18], 31: [42, 20], 34: [45, 23], 36: [47, 25], 38: [39, 49], 39: [40, 38], 40: [41, 39, 29], 41: [42, 40], 42: [41, 53, 31], 45: [56, 34], 47: [58, 36], 49: [60, 38], 53: [64, 42], 56: [67, 45], 58: [69, 47], 60: [71, 49], 62: ['S'], 64: [75, 53], 67: [78, 56], 69: [80, 58], 71: [82, 60], 'S': [84, 62], 75: [86, 64], 78: [89, 67], 80: [91, 69], 82: [71], 84: [95, 'S'], 86: [97, 75], 89: [100, 78], 91: [102, 80], 95: [106, 84], 97: [108, 86], 100: [101, 89], 101: [102, 100], 102: [103, 101, 91], 103: [104, 102], 104: [105, 103], 105: [106, 104], 106: [107, 105, 95], 107: [108, 106], 108: [107, 97]}
+  read_graph = ReadMaze("1.in")
+  print(read_graph.graph)
+  graph = {
+    "A": ['S'],
+    "B": ['C', 'D','S'],
+    "C": ['B', 'J'],
+    "D": ['B', 'G', 'S'],
+    "E": ['G', 'S'],
+    "F": ['G', 'H'],
+    "G": ['D', 'E', 'F', 'H', 'J'],
+    "H": ['F', 'G', 'I'],
+    "I": ['H', 'J'],
+    "J": ['C', 'G', 'I'],
+    "S": ['A', 'B', 'D', 'E']
+  }
 
   G = GraphVisualization()
   for key, value in graph.items():
--- maze_generator/read_maze.py
+++ maze_generator/read_maze.py
@@ -1,71 +1,73 @@
-from node import Node, NodeType
+from matplotlib.pyplot import cla
+from maze_generator.node import Node, NodeType
 
-nodesMatrix = []
-with open('mazes/1.in') as f:
-    lines = f.read().splitlines()
+class ReadMaze():
+    def __init__(self, path):
+        self.nodes_matrix = []
+        self.lines = self.read_maze("mazes/" + path)
+        self.coords = self.lines[0].split(' ')
+        self.x_len = int(self.coords[0])
+        self.y_len = int(self.coords[1])
+        self.graph = {}
+        self.lines.pop(0)
+        self.initialize_nodes(self.lines)
+        self.add_neighbours()
+        
+        for line in self.nodes_matrix:
+            for node in line:
+                print(node.name, end =" ")
+            print()
 
-coords = lines[0].split(' ')
-xLen = int(coords[0])
-yLen = int(coords[1])
-index = 0
-lines.pop(0)
+        for line in self.nodes_matrix:
+            for node in line:
+                print(node.type.name, end =" ")
+            print()
 
-for line in lines:
-    nodes = []
-    for char in line:
-        if(char == 'S'):
-            node = Node('S', NodeType.START)
-        elif(char == 'T'):
-            node = Node('T', NodeType.TARGET)
-        elif(char == '.'):
-            node = Node(index, NodeType.PATH)
-        elif(char == '#'):
-            node = Node(index, NodeType.WALL)
-        else:
-            continue
-        nodes.append(node)
-        index += 1
-    nodesMatrix.append(nodes)
+    
+    def read_maze(self, path):
+        with open(path) as f:
+            return f.read().splitlines()
 
-for x in range(xLen - 1):
-    if(x == 0):
-        continue
-    for y in range(yLen - 1):
-        if(y == 0):
-            continue
-        if(nodesMatrix[x][y].type != NodeType.WALL):
-            if(nodesMatrix[x][y + 1].type != NodeType.WALL):
-                nodesMatrix[x][y].add_neighbour(nodesMatrix[x][y + 1])
-            if(nodesMatrix[x][y - 1].type != NodeType.WALL):
-                nodesMatrix[x][y].add_neighbour(nodesMatrix[x][y - 1])
-            if(nodesMatrix[x + 1][y].type != NodeType.WALL):
-                nodesMatrix[x][y].add_neighbour(nodesMatrix[x + 1][y])
-            if(nodesMatrix[x - 1][y].type != NodeType.WALL):
-                nodesMatrix[x][y].add_neighbour(nodesMatrix[x - 1][y])
 
-for line in nodesMatrix:
-    for node in line:
-        print(node.name, end =" ")
-    print()
+    def initialize_nodes(self, lines):
+        index = 0
+        for line in lines:
+            nodes = []
+            for char in line:
+                if(char == 'S'):
+                    node = Node('S', NodeType.START)
+                elif(char == 'T'):
+                    node = Node('T', NodeType.TARGET)
+                elif(char == '.'):
+                    node = Node(index, NodeType.PATH)
+                elif(char == '#'):
+                    node = Node(index, NodeType.WALL)
+                else:
+                    continue
+                nodes.append(node)
+                index += 1
+            self.nodes_matrix.append(nodes)
 
-for line in nodesMatrix:
-    for node in line:
-        print(node.type.name, end =" ")
-    print()
 
-# for line in nodesMatrix:
-#     for node in line:
-#         print(f"Neighbours of node{node.name}")
-#         for neighbour in node.neighbours:
-#             print(neighbour.name, end =" ")
-#     print()
-
-graph = {}
-for line in nodesMatrix:
-    for node in line:
-        if(node.type != NodeType.WALL):
-            graph[node.name] = []
-            for neighbour in node.neighbours:
-                graph[node.name].append(neighbour.name)
-
-print(graph)
+    def add_neighbours(self):
+        for x in range(self.x_len - 1):
+            if(x == 0):
+                continue
+            for y in range(self.y_len - 1):
+                if(y == 0):
+                    continue
+                if(self.nodes_matrix[x][y].type != NodeType.WALL):
+                    if(self.nodes_matrix[x][y + 1].type != NodeType.WALL):
+                        self.nodes_matrix[x][y].add_neighbour(self.nodes_matrix[x][y + 1])
+                    if(self.nodes_matrix[x][y - 1].type != NodeType.WALL):
+                        self.nodes_matrix[x][y].add_neighbour(self.nodes_matrix[x][y - 1])
+                    if(self.nodes_matrix[x + 1][y].type != NodeType.WALL):
+                        self.nodes_matrix[x][y].add_neighbour(self.nodes_matrix[x + 1][y])
+                    if(self.nodes_matrix[x - 1][y].type != NodeType.WALL):
+                        self.nodes_matrix[x][y].add_neighbour(self.nodes_matrix[x - 1][y])
+        for line in self.nodes_matrix:
+            for node in line:
+                if(node.type != NodeType.WALL):
+                    self.graph[node.name] = []
+                    for neighbour in node.neighbours:
+                        self.graph[node.name].append(neighbour.name)
